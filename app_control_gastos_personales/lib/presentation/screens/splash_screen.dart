47b93c31fae779b330f0efd07c00445d878d545b
exgarciam14 <exgarciam@unah.hn>
2025-08-02 19:10:03 -0600

    Agrego Logo

--- app_control_gastos_personales/lib/presentation/screens/splash_screen.dart
+++ app_control_gastos_personales/lib/presentation/screens/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart'; // Importa el paquete
 import 'package:app_control_gastos_personales/utils/session_controller.dart';
 import 'package:app_control_gastos_personales/config/theme/app_theme.dart';
 import 'package:go_router/go_router.dart';
@@ -13,30 +14,28 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
- @override
-void initState() {
-  super.initState();
-
-  Timer(const Duration(seconds: 2), () {
-    final session = SessionController.instance;
-
-    if (session.userId == null) {
-      context.go('/login'); 
-    } else {
-      context.go('/home');
-    }
-  });
-}
-
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      final session = SessionController.instance;
+      context.go(session.userId == null ? '/login' : '/home');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppTheme.blancoPalido,
+      backgroundColor: AppTheme.verde,
       body: Center(
-        child: Center(
-          child: Text('Splash Screen', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: AppTheme.verdeOscuro)),
+        child: SvgPicture.asset(
+          'assets/svg/logo_light.svg', // Ruta SVG
+          width: 900,
+          height: 900,
+          colorFilter: ColorFilter.mode(
+            AppTheme.verdeOscuro, 
+            BlendMode.srcIn,
+          ),
         ),
       ),
     );
