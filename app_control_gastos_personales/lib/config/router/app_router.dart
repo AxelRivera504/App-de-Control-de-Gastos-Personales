1ac64ea135f95d98382f771d21ef0436930778a8
exgarciam14 <exgarciam@unah.hn>
2025-08-05 12:50:11 -0600

    splash initial location

--- app_control_gastos_personales/lib/config/router/app_router.dart
+++ app_control_gastos_personales/lib/config/router/app_router.dart
@@ -4,7 +4,7 @@ import 'package:app_control_gastos_personales/presentation/screens/screens.dart'
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/main',
+  initialLocation: '/splash',
   routes: [
 
     //Ruta SplashScreen
