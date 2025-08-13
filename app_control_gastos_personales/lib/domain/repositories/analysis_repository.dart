33ca832a76cfd36785ae3be8cf14c918b5ef345b
AxelRivera504 <125411077+AxelRivera504@users.noreply.github.com>
2025-08-12 22:52:46 -0600

    Revert "Agrego cambios de analysis"
    This reverts commit 6aadddde8edfc62ecef1538d5f32b721365f946d.

--- app_control_gastos_personales/lib/domain/repositories/analysis_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/analysis.dart';
-
-abstract class AnalysisRepository {
-  Future<List<Analysis>> getAllAnalyses();
-  Future<Analysis> getAnalysisById(String id);
-  Future<void> createAnalysis(Analysis analysis);
-  Future<void> updateAnalysis(Analysis analysis);
-  Future<void> deleteAnalysis(String id);
-}
